f2c6052f7da19fa45a33721036ebc4761ba44ea2
Lafouc1 <nathfoucault17220@gmail.com>
2025-11-12 15:23:06 +0100

    Ajoutdenouvelles routes

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,6 +4,8 @@
 //console.log ("Hello World!");
 // Définir l'emplacement des fichiers bases de données
 const POKEDEX_SRC = "./DATA/pokedex.json";
+// Définir l'emplacement des types
+const TYPES_SRC = "./DATA/types.json";
 // Définir l'emplacement des images
 const IMAGES_SRC = "./FILES/images";
 // Définir un port
@@ -25,7 +27,7 @@ app.listen(
 )
 
 //Route tous les pokemons
-app.get('/', (req, res) => {
+app.get('/pokemon', (req, res) => {
   // Ouvrir le fichier
   fs.readFile(POKEDEX_SRC, 'utf8', (err, data) => {
     if (err) {
@@ -38,6 +40,21 @@ app.get('/', (req, res) => {
   });
 });
 
+//Route par ID du pokemon
+app.get('/pokemon/:id', (req, res) => {
+  const pokedex = JSON.parse(fs.readFileSync(POKEDEX_SRC, 'utf8'));
+  const id = parseInt(req.params.id, 10);
+  const pokemon = pokedex.find(p => p.id === id);
+
+  //Ajout du lien vers l'image dans le JSON renvoyé
+  if (pokemon) {
+    pokemon.image = `${IMAGES_SRC}/${pokemon.id}.png`;
+  }
+  console.log(pokemon);
+
+  res.send(pokemon);
+
+});
 
 //Route hasard
 app.get('/hasard', (req, res) => {
@@ -55,18 +72,30 @@ app.get('/hasard', (req, res) => {
   });
 });
 
-//Route par ID du pokemon
-app.get('/:id', (req, res) => {
-  const pokedex = JSON.parse(fs.readFileSync(POKEDEX_SRC, 'utf8'));
-  const id = parseInt(req.params.id, 10);
-  const pokemon = pokedex.find(p => p.id === id);
-  res.send(pokemon);
-});
-
 //Route par nom français du pokemon
 app.get('/name/french/:name', (req, res) => {
   const pokedex = JSON.parse(fs.readFileSync(POKEDEX_SRC, 'utf8'));
   const name = req.params.name;
   const pokemon = pokedex.find(p => p.name.french === name);
   res.send(pokemon);
+});
+
+//Route par type du pokemon
+app.get('/type/:type', (req, res) => {
+  const pokedex = JSON.parse(fs.readFileSync(POKEDEX_SRC, 'utf8'));
+  const types = JSON.parse(fs.readFileSync(TYPES_SRC, 'utf8'));
+  const type = req.params.type;
+  const typeInfo = types.find(t => t.english === type);
+  if (!typeInfo) {
+    res.status(404).send('Type not found');
+    return;
+  }
+  const pokemon = pokedex.filter(p => p.type.includes(typeInfo.english));
+  res.send(pokemon);
+});
+
+//Route pour tous les types
+app.get('/types', (req, res) => {
+  const types = JSON.parse(fs.readFileSync(TYPES_SRC, 'utf8'));
+  res.send(types);
 });
